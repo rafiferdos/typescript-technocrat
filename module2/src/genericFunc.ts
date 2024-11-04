a2c66104a2a240e2d352c533069e048420cedcce
Rafi Ferdos <rafiferdos@gmail.com>
2024-11-04 16:07:13 +0600

    add generic function to assign a course to a student

---
+++ module2/src/genericFunc.ts
@@ -0,0 +1,7 @@
+const addCourseToStudent = <T> (student: T) => {
+    const course = 'NLWD'
+    return {...student, course}
+}
+
+const student1 = addCourseToStudent({name: 'Rafi', email: 'x@y.com', age: 22})
+console.log(student1)
