184de660fd0ece35e66810f90b9a0b5ff262e437
Rafi Ferdos <rafiferdos@gmail.com>
2024-11-05 23:13:27 +0600

    add generic function to add a course to a student with constraints and examples

---
+++ module2/src/constraints.ts
@@ -0,0 +1,45 @@
+const addCourseToStudent = <
+  T extends { id: number; name: string; email: string }
+>(
+  student: T
+) => {
+  const course = "NLWD";
+  return { ...student, course };
+};
+
+const student1 = addCourseToStudent({
+  id: 1234,
+  name: "Rafi",
+  email: "x@y.com",
+  age: 22,
+});
+const student2 = addCourseToStudent({
+  id: 123,
+  name: "Rafi",
+  email: "x@y.com",
+  age: 22,
+});
+const student3 = addCourseToStudent({
+  id: 12,
+  name: "Ferdos",
+  email: "xs@gmail.com",
+  destiny: "toAllah",
+});
+console.log(student1);
+
+// using keyof
+
+type Computer = {
+    Windows: string;
+    Mac: string;
+    Linux: string;
+}
+type User = 'Windows' | 'Mac' | 'Linux' //manually
+type UserNew = keyof Computer //auto
+
+const person1 : User = 'Linux'
+const person2 : UserNew = 'Mac'
+
+const getPropertyKey = <X, Y extends keyof X>(obj: X, key: Y) => {
+    return obj[key]
+}
--- module2/src/genericFunc.ts
+++ module2/src/genericFunc.ts
@@ -1,7 +1,13 @@
-const addCourseToStudent = <T> (student: T) => {
-    const course = 'NLWD'
-    return {...student, course}
-}
+{
+  const addCourseToStudent = <T>(student: T) => {
+    const course = "NLWD";
+    return { ...student, course };
+  };
 
-const student1 = addCourseToStudent({name: 'Rafi', email: 'x@y.com', age: 22})
-console.log(student1)
+  const student1 = addCourseToStudent({
+    name: "Rafi",
+    email: "x@y.com",
+    age: 22,
+  });
+  console.log(student1);
+}
