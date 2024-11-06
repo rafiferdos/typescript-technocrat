fa1cbea2330ca46d42816694ed7941dc487da34e
Rafi Ferdos <rafiferdos@gmail.com>
2024-11-06 16:24:31 +0600

    refactor type definitions for improved clarity and structure; update output directory in tsconfig and add initial OOP example

--- module2/src/genericWithInterface.ts
+++ module2/src/genericWithInterface.ts
@@ -1,54 +1,56 @@
-interface Developer<X, Y = null> {
-  name: string;
-  post: string;
-  age: number;
-  home: {
-    presentAddress: string;
-    permanentAddress?: string;
+{
+  interface Developer<X, Y = null> {
+    name: string;
+    post: string;
+    age: number;
+    home: {
+      presentAddress: string;
+      permanentAddress?: string;
+    };
+    smartPhone: X;
+    smartWatch?: Y;
+  }
+
+  interface smartPhone {
+    brand: string;
+    model: string;
+  }
+  interface smartWatch extends smartPhone {
+    heartbitSensor: boolean;
+    SPO2Meter: boolean;
+  }
+
+  // developer initialization
+  const lowClassDev: Developer<smartPhone> = {
+    name: "Rafi",
+    post: "Frontend Developer",
+    age: 22,
+    home: {
+      presentAddress: "Dhaka",
+    },
+    smartPhone: {
+      brand: "xiaomi",
+      model: "k50i",
+    },
   };
-  smartPhone: X;
-  smartWatch?: Y;
-}
 
-interface smartPhone {
-  brand: string;
-  model: string;
+  const middleClassDev: Developer<smartPhone, smartWatch> = {
+    name: "Ferdos",
+    post: "MERN-Stack Developer",
+    age: 21,
+    home: {
+      presentAddress: "Dhaka",
+      permanentAddress: "Chapai Nawabganj",
+    },
+    smartPhone: {
+      brand: "Samsung",
+      model: "s24",
+    },
+    smartWatch: {
+      brand: "Amazefit",
+      model: "gtr mini 7",
+      heartbitSensor: true,
+      SPO2Meter: false,
+    },
+  };
 }
-interface smartWatch extends smartPhone {
-  heartbitSensor: boolean;
-  SPO2Meter: boolean;
-}
-
-// developer initialization
-const lowClassDev: Developer<smartPhone> = {
-  name: "Rafi",
-  post: "Frontend Developer",
-  age: 22,
-  home: {
-    presentAddress: "Dhaka",
-  },
-  smartPhone: {
-    brand: "xiaomi",
-    model: "k50i",
-  },
-};
-
-const middleClassDev: Developer<smartPhone, smartWatch> = {
-  name: "Ferdos",
-  post: "MERN-Stack Developer",
-  age: 21,
-  home: {
-    presentAddress: "Dhaka",
-    permanentAddress: "Chapai Nawabganj",
-  },
-  smartPhone: {
-    brand: "Samsung",
-    model: "s24",
-  },
-  smartWatch: {
-    brand: "Amazefit",
-    model: "gtr mini 7",
-    heartbitSensor: true,
-    SPO2Meter: false,
-  },
-};
